785048122dda93c9aea87decff8f8ee1ac08ba66
kuldeepabsolute-glitch <kuldeep.absolute@genus.in>
2025-09-19 15:28:13 +0530

    games 5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,51 +5,42 @@ import { LoginGuard } from './auth/login.guard';
 
 export const routes: Routes = [
 	{ path: 'login', loadComponent: () => import('./auth/login.component').then(m => m.LoginComponent), canActivate: [LoginGuard] },
-	{ path: '', component: Home, canActivate: [AuthGuard] },
+	{ path: '', component: Home },
 	{
 		path: 'todo',
-		loadComponent: () => import('./todo/todo-list').then(m => m.ToDoList),
-		canActivate: [AuthGuard]
+		loadComponent: () => import('./todo/todo-list').then(m => m.ToDoList)
 	},
 	{
 		path: 'users',
-		loadComponent: () => import('./users/users-list.component').then(m => m.UsersListComponent),
-		canActivate: [AuthGuard]
+		loadComponent: () => import('./users/users-list.component').then(m => m.UsersListComponent)
 	},
 	{
 		path: 'weather',
-		loadComponent: () => import('./weather/weather-dashboard').then(m => m.WeatherDashboard),
-		canActivate: [AuthGuard]
+		loadComponent: () => import('./weather/weather-dashboard').then(m => m.WeatherDashboard)
 	},
 	{
 		path: 'news',
-		loadComponent: () => import('./news/news-list.component').then(m => m.NewsListComponent),
-		canActivate: [AuthGuard]
+		loadComponent: () => import('./news/news-list.component').then(m => m.NewsListComponent)
 	},
 	{
 		path: 'news/article',
-		loadComponent: () => import('./news/news-detail.component').then(m => m.NewsDetailComponent),
-		canActivate: [AuthGuard]
+		loadComponent: () => import('./news/news-detail.component').then(m => m.NewsDetailComponent)
 	},
 	{
 		path: 'tic-tac-toe',
-		loadChildren: () => import('./tic-tac-toe/tic-tac-toe.module').then(m => m.TicTacToeModule),
-		canActivate: [AuthGuard]
+		loadChildren: () => import('./tic-tac-toe/tic-tac-toe.module').then(m => m.TicTacToeModule)
 	},
 	{
 		path: 'snake-game',
-		loadChildren: () => import('./snake-game/snake-game.module').then(m => m.SnakeGameModule),
-		canActivate: [AuthGuard]
+		loadChildren: () => import('./snake-game/snake-game.module').then(m => m.SnakeGameModule)
 	},
 	{
 		path: 'flappy-bird',
-		loadChildren: () => import('./flappy-bird/flappy-bird.module').then(m => m.FlappyBirdModule),
-		canActivate: [AuthGuard]
+		loadChildren: () => import('./flappy-bird/flappy-bird.module').then(m => m.FlappyBirdModule)
 	},
 	{
 		path: 'reaction-time',
-		loadChildren: () => import('./reaction-time/reaction-time.module').then(m => m.ReactionTimeModule),
-		canActivate: [AuthGuard]
+		loadChildren: () => import('./reaction-time/reaction-time.module').then(m => m.ReactionTimeModule)
 	},
-	{ path: '**', redirectTo: '/login' }
+	{ path: '**', redirectTo: '/' }
 ];
